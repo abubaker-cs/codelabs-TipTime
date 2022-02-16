1f3dc97f4a0385029328154bba1dc96fb3bd94da
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-02-17 00:03:07 +0500

    Project completed

--- app/src/main/java/org/abubaker/tiptime/MainActivity.kt
+++ app/src/main/java/org/abubaker/tiptime/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import org.abubaker.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +15,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Inflate the activity_main.xml file
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        // mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        mBinding.calculateButton.setOnClickListener {
+            calculateTip()
+        }
+    }
+
+    private fun calculateTip() {
+
+        val stringInTextField = mBinding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+
+        if (cost == null) {
+            mBinding.tipResult.text = ""
+            return
+        }
+
+        val tipPercentage = when (mBinding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+
+        if (mBinding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        mBinding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
     }
 }
